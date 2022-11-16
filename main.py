0c5259467fcd93e83f0df681ee8f32dfa353baaa
Owen <gw10plimerowen2@glow.sch.uk>
2022-11-16 10:00:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+import random, os
 from flask import Flask
 from flask import render_template as rt
 
@@ -9,4 +9,4 @@ def base():
   return rt('base.html', )
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=random.randint(2000, 9000))
+  app.run(host='0.0.0.0', port=os.getenv("PORT", default=5000))
